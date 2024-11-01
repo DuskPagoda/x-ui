4782a165f34ed0ee3724ea3b3ab7ab16aa4b5881
dusk <73323108+DuskPagoda@users.noreply.github.com>
2024-11-01 09:52:19 +0800

    Update x-ui.sh

--- Linux/x-ui.sh
+++ Linux/x-ui.sh
@@ -270,8 +270,8 @@ check_status() {
     if [[ ! -f /etc/systemd/system/x-ui.service ]]; then
         return 2
     fi
-    temp=$(systemctl status x-ui | grep Active | awk '{print \$3}' | cut -d "(" -f2 | cut -d ")" -f1)
-    if ([[ x"${temp}" == x"running" ]]); then
+    temp=$(systemctl status x-ui | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
         return 0
     else
         return 1
